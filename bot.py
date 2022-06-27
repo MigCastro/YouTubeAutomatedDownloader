f3fa2c09f9154b612d7f1dfdba3f799efe0d80f3
migcastro <mcastro32902@gmail.com>
2022-06-26 21:02:34 -0400

    Comments

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
 from pytube import YouTube
 import os
 
-linkFile = open("/Users/miguelcastro/Documents/YouTubeMP3Automation-main/links.txt", "r")
+#Opens your /links.txt file.
+linkFile = open("/Users/miguelcastro/Documents/YouTubeMP3Automation/links.txt", "r")
 print("Welcome to the YouTube Video/Audio downloader.")
+
+#This is where you type in your destination folder for the media you want to download.
 print("Enter the destination address (leave blank to save in current directory)")
 destination = str(input(" ")) or '.'
+
+#Input the media you would like to download.
 print("Please enter the type of media you will be downloading.")
 print("Video or Audio?")
 media = str(input(" "))
+#Audio encoder
 if media == str("Audio") or media == str("audio"):
     print("Downloading Audio.")
     for line in linkFile:
@@ -19,9 +25,11 @@ if media == str("Audio") or media == str("audio"):
                 base, ext = os.path.splitext(out_file)
                 new_file = base + '.mp3'
                 os.replace(out_file, new_file)
-            print("Downloaded " + yt.title)        
+            print("Downloaded " + yt.title)
             linkFile.close
     print("All Files have been successfully downloaded.")
+
+#Video encoder
 elif media == str("Video") or media == str("video"):
     print("Downloading Video.")
     for line in linkFile:
@@ -36,6 +44,8 @@ elif media == str("Video") or media == str("video"):
             print("Downloaded " + yt.title)
             linkFile.close
     print("All files have been successfully downloaded.")
+
+#If NOT Video or Audio, error.
 else:
-    print("Incorrect response")            
+    print("Incorrect response")
 
